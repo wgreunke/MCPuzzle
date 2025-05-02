edf8cf7fdb7786d28e1ccadb0b8a7451002edfcd
Ward Greunke <wgreunke@gmail.com>
2025-05-02 13:08:59 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Create a root for the React application
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside the root element
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, you can use the following code:
+// const reportWebVitals = onPerfEntry => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+// reportWebVitals();
